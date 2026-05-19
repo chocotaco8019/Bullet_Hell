ea982ce2ad0e6bdef4e299fc40664ba906637a23
Noah Rider <media@noahrider.com>
2026-05-19 10:40:05 -0400

    highscores

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import json
 import sys
 
 from Object import Object
@@ -10,14 +11,20 @@ SCREEN_HEIGHT = 480
 
 pygame.init()
 pygame.font.init()
+pygame.mixer.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 running = True
 dt = 0
 
+channelMusic = pygame.mixer.Channel(1)
+channelSFX = pygame.mixer.Channel(2)
+
 Vector2 = pygame.math.Vector2
 
 fntMain = pygame.font.Font("assets/alagard.ttf", 32)
+
+#musLevel1 = pygame.sound.Sound("assets/")
 
 __current_draw_color__ = pygame.Color(255, 255, 255, 255)
 
@@ -27,8 +34,39 @@ deaths = 0
 killed = False
 
 score = 0
-hiscore = 0
+hiscores = 0
+hiscoresJsonData = {}
 
+#load hiscores
+fileid = open("hiscores.json")
+hiscoresJsonData = fileid.read()
+fileid.close()
+
+hiscores = json.loads(hiscoresJsonData)["scores"]
+
+print(hiscores)
+
+def GetScoreAtIndex(idx = 0):
+    _scores = []
+    _names = []
+    for player in hiscores:
+        _scores.append(player['score'])
+        _names.append(player['name'])
+    
+    _scores.sort(reverse=True) #reversi
+    return json.loads("{\"name\": \"" + str(_names[idx]) + "\", \"score\": " + str(_scores[idx]) + "}")
+
+def SaveScore(name, score = score):
+    global hiscores
+    fileid = open("hiscores.json", "w")
+    hiscores.append(json.loads("{\"name\": \"" + str(name) + "\", \"score\": " + str(score) + "}"))
+    fileid.write(json.dumps(hiscores))
+    fileid.close()
+
+    hiscores = json.loads(hiscoresJsonData)["scores"]
+    pass
+
+print(GetScoreAtIndex())
 
 def ButtonPressed(key):
     return pygame.key.get_pressed()[key]
@@ -40,8 +78,9 @@ def DrawText(x, y, string, color = __current_draw_color__, font = fntMain):
     __textSurface = font.render(string, False, color)
     screen.blit(__textSurface, (x, y))
 
-def DrawSprite(sprite, x, y):
+def DrawSprite(sprite, x, y, scale = (1, 1)):
     __imageSurface = sprite
+    pygame.transform.scale(__imageSurface, scale)
     screen.blit(__imageSurface, (x, y))
 
 def ChangeScene(scn):
@@ -50,24 +89,19 @@ def ChangeScene(scn):
 
 def TimeToDie():
     global score
-    global hiscore
     global player
     global deaths
-    global killed
 
-    if killed == True: pass
     if not scene.instanceExists(player): pass
     if player.invincible: pass
 
-    killed = True
-    #deaths += 1
+    deaths += 1
     if deaths < 3:
         scene.destroyInstance(player)
-        PlayerDeathEvent()
+        scene.addInstance(PlayerDeathEvent())
     else:
-        print(f"died! score: {score} (hiscore: {hiscore}), deaths: {deaths}")
-        pygame.quit()
-        sys.exit(-2147483647)
+        print(f"died! score: {score} (hiscore: {GetScoreAtIndex()})")
+        scene.addInstance(GameOverEvent())
 
 class IntroMenu(Object):
     def __init__(self, x, y):
@@ -81,6 +115,9 @@ class IntroMenu(Object):
 
     def render(self):
         DrawText(20, 20, "press Z key to continue")
+
+        for i in range(len(hiscores)):
+            DrawText(50, 50 + (30 * i), GetScoreAtIndex(i)["name"] + "-" + str(GetScoreAtIndex(i)["score"]))
         
 class PlayerObject(Object):
     def __init__(self, x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT - 48):
@@ -119,8 +156,8 @@ class PlayerObject(Object):
         self.position.y += self.my
 
     def render(self):
-        if not self.invincible: DrawSprite(sprPlayer, self.position.x, self.position.y)
-        elif self.timer % 2 == 0: DrawSprite(sprPlayer, self.position.x, self.position.y)
+        if not self.invincible: DrawSprite(sprPlayer, self.position.x, self.position.y, (0.5, 0.5))
+        elif self.timer % 2 == 0: DrawSprite(sprPlayer, self.position.x, self.position.y, (0.5, 0.5))
 
 class BulletObject(Object):
     def __init__(self, x, y, velocity = Vector2(0, 0)):
@@ -133,13 +170,13 @@ class BulletObject(Object):
         super().update(dt)
         self.bbox = pygame.Rect(self.position.x - 4, self.position.y - 4, 8, 8)
 
-        if not scene.instanceExists(player): pass
+        if not scene.instanceExists(player): return
 
         if self.bbox.colliderect(player.bbox):
             TimeToDie()
         if not self.bbox.colliderect(pygame.Rect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)) and self.destroyOOB:
             scene.destroyInstance(self)
-        pass
+            
     def render(self):
         pygame.draw.circle(screen, (255, 255, 255), self.position, 8)
 
@@ -169,7 +206,7 @@ class EnemyObject(Object):
         super().update(dt)
         self.bbox = pygame.Rect(self.position.x - 8, self.position.y - 8, 16, 16)
         
-        if not scene.instanceExists(player): pass
+        if not scene.instanceExists(player): return
 
         if self.bbox.colliderect(player.bbox):
             TimeToDie()
@@ -188,12 +225,13 @@ class PlayerDeathEvent(Object):
 
     def update(self, dt):
         super().update(dt)
-        self.timer += 1
+        self.timer += 1 * dt
 
-        if self.timer >= 5:
+        if self.timer >= 3:
             global player
             player = PlayerObject()
             scene.addInstance(player)
+            scene.destroyInstance(self)
 
 
     def render(self):
@@ -204,6 +242,9 @@ class GameOverEvent(Object):
     def __init__(self, x = 0, y = 0):
         super().__init__(x, y)
         self.timer = 0
+        SaveScore(input("Input your name. "), score)
+        pygame.quit()
+        sys.exit(-2147483647)
 
 
     def update(self, dt):
